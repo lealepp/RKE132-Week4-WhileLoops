551a614e7e9fbe882929d3af321e2455a4663d44
lealepp <lea.lepp@tktk.ee>
2024-01-06 17:16:38 +0200

    add Week4Loops code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿Random rnd = new Random();
+int myRandomNum;
+int randomSum = 0;
+for (int i = 0; i < 10; i++)
+
+{
+    myRandomNum = rnd.Next(0, 11);
+    randomSum = myRandomNum + randomSum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+Console.WriteLine($"random sum total: {randomSum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,5 @@
+﻿int i = 0;
+for (i = 0; i < 5;i++)
+{ Console.WriteLine("We dont need no education.");}
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
